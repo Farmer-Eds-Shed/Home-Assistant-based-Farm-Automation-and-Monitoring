181e89c88300c134639532a80afbdd047ada7dd3
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2021-10-17 20:08:30 +0100

    Update 4ChLoRaRelay.ino

--- MySensors/4ChLoRaRelay.ino
+++ MySensors/4ChLoRaRelay.ino
@@ -1,5 +1,26 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ */
 
-
+//LoRa-Radio-Node-v1.0 (Uses Default MySensors pin config)
 #define MY_DEBUG
 #define MY_RADIO_RFM95
 #define MY_DEBUG_VERBOSE_RFM95
@@ -17,7 +38,7 @@
 #define MY_NODE_ID 110
 
 
-// Enable repeater functionality for this node
+// Enable repeater functionality for this node as relay node will never sleep
 #define MY_REPEATER_FEATURE
 
 #include <SPI.h>
